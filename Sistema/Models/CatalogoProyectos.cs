45323ab891c2bba6543fadde04a8ff1778d84fe0
neri_1319104640@uptecamac.edu.mx <Putosgay01>
2021-10-14 16:31:00 -0500

    v0.1

---
+++ Sistema/Models/CatalogoProyectos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.Models
+{
+    public class CatalogoProyectos
+    {
+        public String NombreProyecto { get; set; }
+        public String ClaveProgramaEducativo { get; set; }
+        public String RFC_UnidadEconomica { get; set; }
+        public String AreaConocimiento { get; set; }
+        public String Etapa { get; set; }
+        public int FechaInicio { get; set; }
+        public int FechaTermino { get; set; }
+        public char Estatus { get; set; }
+        public int NumeroHoras { get; set; }
+        public int FechaCambioEstatus { get; set; }
+
+    }
+}
