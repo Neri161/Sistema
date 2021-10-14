eefc4872334233df5ee8b444f1189bac87d7de69
JimJorge <81451231+JimJorge@users.noreply.github.com>
2021-10-14 16:27:57 -0500

    Cambio

---
+++ Sistema/Models/CatalogoCompetencias.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.Models
+{
+    public class CatalogoCompetencias
+    {
+        public String ClaveAsignatura { get; set; }
+        public String Periodo { get; set; }
+        public String UnidadCompetencia { get; set; }
+        public String NombreAsignatura { get; set; }
+        public String Actividad { get; set; }
+
+    }
+}
---
+++ Sistema/Models/MentorEmpresarial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.Models
+{
+    public class MentorEmpresarial
+    {
+        public int ClaveMentorEmpresarial { get; set; }
+        public String Nombre { get; set; }
+        public String ApellidoPaterno { get; set; }
+        public String ApellidoMaterno { get; set; }
+        public int Telefono { get; set; }
+        public String Correo { get; set; }
+
+    }
+}
