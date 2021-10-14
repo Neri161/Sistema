faaed6061b02b4e3284f03172ba33a1334cd3898
neri_1319104640@uptecamac.edu.mx <Putosgay01>
2021-10-14 16:28:53 -0500

    v0.1

---
+++ Sistema/Models/UnidadesEconomicas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.Models
+{
+    public class UnidadesEconomicas
+    {
+        [Key]
+        public String RFC { get; set; }
+        public String nombreComercial { get; set; }
+        public String cargo { get; set; }
+        public String razonSocial { get; set; }
+        public String colonia { get; set; }
+        public String sector { get; set; }
+        public String localidad { get; set; }
+        public String programaEducativo { get; set; }
+        public int CP { get; set; }
+        public String telefono { get; set; }
+        public String municipio { get; set; }
+        public String representanteLegal { get; set; }
+    }
+}
