43221b4b6b1bd2185c4485def1ca9b379509e398
Alfred-in <alfredo_1319104653@uptecamac.edu.mx>
2021-10-21 16:47:27 -0500

    universidadController

---
+++ Sistema/Controllers/universidadController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sistema.Models;
+
+namespace Sistema.Controllers
+{
+    public class universidadController : ApiController
+    {
+        [ActionName("inicioSesion")]
+        [HttpPost]
+        public Reply login(Universidad objeto)
+        {
+            using (DB conectar = new DB())
+            {
+                Reply rp = new Reply();
+                try
+                {
+                    Universidad usuarioObtenido = conectar.Universidad.Where(x => x.ClaveUniversidad == objeto.ClaveUniversidad).First();
+                    if (usuarioObtenido.ClaveUniversidad == objeto.ClaveUniversidad)
+                    {
+                        if (usuarioObtenido.Password == objeto.Password)
+                        {
+                            rp.result = 1;
+                            rp.data = Guid.NewGuid().ToString();
+                            rp.message = "Ingreso exitoso";
+                            //rp.ID = usuarioObtenido.IdUsuario;
+                            //usuarioObtenido.Token_ = (String)rp.data;
+                            conectar.Entry(usuarioObtenido).State = System.Data.Entity.EntityState.Modified;
+                            conectar.SaveChanges();
+                            return rp;
+                        }
+                        else
+                        {
+                            rp.result = 0;
+                            rp.message = "Datos incorrectos";
+                            return rp;
+                        }
+                    }
+                    else
+                    {
+                        rp.result = 0;
+                        rp.message = "Usuario inexistente";
+                        return rp;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    rp.result = 0;
+                    rp.message = "Ha ocurrido un error: "+ex;
+                    return rp;
+                }
+            }
+
+        }
+    }
+}
