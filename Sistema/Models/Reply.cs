300938758619d4a5a4a41a803c5cbfdd642b555b
neri_1319104640@uptecamac.edu.mx <Putosgay01>
2021-10-21 15:30:58 -0500

    v0.2

---
+++ Sistema/Models/Reply.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.Models
+{
+    public class Reply
+    {
+        public int result { get; set; }
+        public object data { get; set; }
+        public String message { get; set; }
+        public int ID { get; set; }
+    }
+}
